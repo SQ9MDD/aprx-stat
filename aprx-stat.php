8de54db46765d7af67e33965fea6ce8efba96640
SQ9MDD <rysieklabus@gmail.com>
2017-01-31 08:14:26 +0100

    v.1.3
    variable callsign length fix thanx KG4PID

--- aprx-stat.php
+++ aprx-stat.php
@@ -6,7 +6,7 @@
  SQ9MDD rysieklabus (at) gmail (dot) com
  
  CHANGELOG
- 2017.01.31 multilanguage support
+ 2017.01.31 multilanguage support and v.1.3 variable callsign length fix thanx KG4PID 
 */
 //*****CONFIG******
 $callsign = "SQ9MDD";								//your callsign
@@ -18,7 +18,7 @@ $secondary_interface = "SQ9MDD-2";					//your callsign and ssid for secondary in
 $secondary_interface_frequency = "432.500MHz";		//frequency for secondary interface
 
 $aprx_log_db = file('/var/log/aprx/aprx-rf.log');	//aprx log-file full path 
-
+//$aprx_log_db = file('/tmp/aprx1-rf.log');
 $language	=	'EN';								//default PL -polish, EN -english
 
 //***END CONFIG****
@@ -68,23 +68,72 @@ function interface_traffic(array $wszystkie){
 echo "<pre>";
 $ilosc_wszystkich_ramek = count($aprx_log_db);
 
+//KG4PID-14	<- longest 9
+//SQ9M		<-shortest 4
+$pri_call_len = strlen($primary_interface);
+$sec_call_len = strlen($secondary_interface);
+
+switch($pri_call_len){
+	case(9):
+		$primary_interface = $primary_interface;
+	break;
+	case(8):
+		$primary_interface = $primary_interface." ";
+	break;
+	case(7):
+		$primary_interface = $primary_interface."  ";
+	break;	
+	case(6):
+		$primary_interface = $primary_interface."   ";
+	break;	
+	case(5):
+		$primary_interface = $primary_interface."    ";
+	break;	
+	case(4):
+		$primary_interface = $primary_interface."     ";
+	break;	
+}
+
+switch($sec_call_len){
+	case(9):
+		$secondary_interface = $secondary_interface;
+	break;
+	case(8):
+		$secondary_interface = $secondary_interface." ";
+	break;
+	case(7):
+		$secondary_interface = $secondary_interface."  ";
+	break;	
+	case(6):
+		$secondary_interface = $secondary_interface."   ";
+	break;	
+	case(5):
+		$secondary_interface = $secondary_interface."    ";
+	break;	
+	case(4):
+		$secondary_interface = $secondary_interface."     ";
+	break;	
+}
+
 for($a=0;$a<=(count($aprx_log_db)-1);$a++){
 	$linia = $aprx_log_db[$a];
 	if(strpos($linia,"$callsign-") == true){
 		$ramki_radiowe[] = $linia;
-		if(strpos($linia,"$primary_interface  R") == true){				//2m odebrane			
-			$linia_arr_2m = explode('>',$linia);						
-			$linia_znak_2m[] = substr($linia_arr_2m[0],36);				//znaki na tym interface
+		if((strpos($linia,"$primary_interface R") == true) OR (strpos($linia,"$primary_interface d") == true)){				//2m odebrane			
+			$linia_arr_2m = explode('>',$linia);	
+			$tmp_to_clenup_2m = substr($linia_arr_2m[0],36);
+			$linia_znak_2m[] = str_replace("*","",$tmp_to_clenup_2m);				//znaki na tym interface
 			$ramki_iface_one_arr[] = $linia;							//ramki na tym interface
-		}else if(strpos($linia,"$secondary_interface  R") == true){ 	//70cm odebrane			
+		}else if((strpos($linia,"$secondary_interface R") == true) OR (strpos($linia,"$secondary_interface d") == true)){ 	//70cm odebrane			
 			$linia_arr_70cm = explode('>',$linia);
-			$linia_znak_70cm[] = substr($linia_arr_70cm[0],36);						
+			$tmp_to_clenup_70cm = substr($linia_arr_70cm[0],36);
+			$linia_znak_70cm[] = str_replace("*","",$tmp_to_clenup_70cm);						
 			$ramki_iface_two_arr[] = $linia;							
-		}else if(strpos($linia,"$primary_interface  T") == true){ 		//2m powtorzone
+		}else if(strpos($linia,"$primary_interface T") == true){ 		//2m powtorzone
 			$linia_arr_2m_tx = explode('>',$linia);
 			$linia_znak_2m_tx[] = substr($linia_arr_2m_tx[0],36);	
 			$ramki_iface_one_tx_arr[] = $linia;
-		}else if(strpos($linia,"$secondary_interface  T") == true){		//70cm powtórzone
+		}else if(strpos($linia,"$secondary_interface T") == true){		//70cm powtórzone
 			$linia_arr_70cm_tx = explode('>',$linia);
 			$linia_znak_70cm_tx[] = substr($linia_arr_70cm_tx[0],36);			
 			$ramki_iface_two_tx_arr[] = $linia;
